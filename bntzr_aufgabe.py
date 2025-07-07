91e67c2f83dcbbdb85cb1daa79a7e80cb31eecbf
MD-43700614776\Student <roymosl3@gmail.com>
2025-07-07 09:43:43 +0200

    projekt

---
+++ bntzr_aufgabe.py
@@ -0,0 +1,5 @@
+benutzer = input("Nutzer Name Auswahlen: ")
+alter = int(input("Wie alt sind Sie: "))
+alt_dpplt = alter * 2
+
+print("Hallo! ", benutzer, "\t Sind Sie wirklich   +" + str(alt_dpplt)+"?")
